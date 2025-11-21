0f3818940405a56f37cf4ef991273c72473efc18
marsiratv <kisalsymbianzgenerationz@gmail.com>
2025-11-21 18:42:05 +0800

    Create app.jsx

---
+++ src/app.jsx
@@ -0,0 +1,24 @@
+18:33:54.303 Running build in Washington, D.C., USA (East) – iad1
+18:33:54.304 Build machine configuration: 2 cores, 8 GB
+18:33:54.751 Cloning github.com/marsiratv/m3u-editor (Branch: main, Commit: fb1e5ed)
+18:33:54.751 Previous build caches not available.
+18:33:54.911 Cloning completed: 159.000ms
+18:33:55.384 Running "vercel build"
+18:33:55.787 Vercel CLI 48.10.5
+18:33:56.372 Installing dependencies...
+18:34:10.394 
+18:34:10.394 added 130 packages in 13s
+18:34:10.394 
+18:34:10.395 25 packages are looking for funding
+18:34:10.395   run `npm fund` for details
+18:34:10.437 Running "npm run build"
+18:34:10.548 
+18:34:10.549 > m3u-editor-pro@1.0.0 build
+18:34:10.549 > vite build
+18:34:10.549 
+18:34:10.836 [36mvite v5.4.21 [32mbuilding for production...[36m[39m
+18:34:10.886 transforming...
+18:34:10.914 [32m✓[39m 4 modules transformed.
+18:34:10.918 [31mx[39m Build failed in 58m
+18:34:10.918 file: [36m/vercel/path0/src/main.jsx[31m
+18:34:10.918     at getRollupError (file:///vercel/path0/node_modules/rollup/dist/es/shared/parseAst.js:401:
